a19fa75197052224e9a88d64bee1dafe535f55fb
yukti10 <ysaluja10@gmail.com>
2021-03-05 20:19:06 -0500

    Student class updated with all attributes

--- src/p1/student.java
+++ src/p1/student.java
@@ -1,4 +1,22 @@
 package p1;
 
+import java.util.Date;
+
 public class student {
+    private int studentId;
+    private String firstName;
+    private String lastName;
+    private Date dateOfBirth;
+    private Gender gender;//enum
+    private String address;
+    private String city;
+    private String pincode;
+    private String cellphone;
+    private String email;
+    private String department;
+    private float marks[];
+    private float totalMarks;
+    private float percentage;
+    private String grade;
+
 }
