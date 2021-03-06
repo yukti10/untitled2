77cc81ef4c444d0b4947710895eb048ac6f76218
yukti10 <ysaluja10@gmail.com>
2021-03-05 21:38:40 -0500

    calculate total, percentage and grade

--- src/p1/main.java
+++ src/p1/main.java
@@ -1,4 +1,13 @@
 package p1;
 
 public class main {
+    public static void main(String[] args) {
+        student s1 = new student(1);
+        float[] YuktiMarks = {80,80,80,80,80};
+        Student Yukti = new Student(780739, "Yukti", "Saluja",
+                new Date(), Gender.FEMALE, "String address", "String city", "String pinCode",
+                "String cellPhone", "String email", "String department", dalveerMarks);        dalveer.calculateGrade();        float[] dwarkeshMarks = {90,90,90,90,90};
+
+
+    }
 }
